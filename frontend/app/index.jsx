03676e4c5778731be6ad56a404e0e091b71c79d4
npoirey <nicolas.poirey@gmail.com>
2016-10-18 15:37:27 +0200

    Initial commit

---
+++ frontend/app/index.jsx
@@ -0,0 +1,56 @@
+const React = require('react');
+const Router = require('react-router');
+
+const log = require('bows')('App');
+
+require('./index.css');
+
+const App = React.createClass({
+  render() {
+    return (
+      <div className="App">
+        <h1>THE NEW APP</h1>
+        <Router.Link to="someview">GO TO SOMEVIEW</Router.Link>
+        <br />
+        <Router.Link to="/">GO TO HOME NOW !</Router.Link>
+        <Router.RouteHandler {...this.props}/>
+      </div>
+    );
+  },
+});
+
+const SomeView = React.createClass({
+  render() {
+    return (
+      <div>
+        Some view
+      </div>
+    );
+  },
+});
+
+const DefaultView = React.createClass({
+  render() {
+    return (
+      <div>
+        Default View
+      </div>
+    );
+  },
+});
+
+const routes = (
+  <Router.Route name="app" path="/" handler={App}>
+    <Router.DefaultRoute handler={DefaultView}/>
+    <Router.Route name="someview" path="/something" handler={SomeView}/>
+
+  </Router.Route>
+);
+
+Router.run(routes, Router.HistoryLocation, (Handler, state) => {
+  log('route change', state);
+
+  // store logic here if required
+
+  React.render(<Handler/>, document.body);
+});
