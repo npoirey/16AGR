a940d24296f1f1f72cbf70410f90fd4f84f4f7b5
npoirey <nicolas.poirey@gmail.com>
2016-12-11 23:54:13 +0100

    Using formsy for form validation

--- frontend/src/js/components/layout/Navbar/Login.js
+++ frontend/src/js/components/layout/Navbar/Login.js
@@ -1,33 +1,58 @@
 import React from "react";
 import {connect} from "react-redux";
 import FlatButton from "material-ui/FlatButton";
-import TextField from "material-ui/TextField";
+import FormsyText from "formsy-material-ui/lib/FormsyText";
 import {login} from "../../../actions/userActions";
 
 @connect()
 export default class Login extends React.Component {
 
-  submit(event) {
-    event.preventDefault();
+  constructor() {
+    super();
+    this.state = {
+      canSubmit: false,
+    };
+  };
+
+  enableButton = () => {
+    this.setState({
+      canSubmit: true,
+    });
+  };
+
+  disableButton = () => {
+    this.setState({
+      canSubmit: false,
+    });
+  };
+
+  submit = () => {
     this.props.dispatch(login(this.refs.email.getValue(), this.refs.password.getValue()));
-  }
+  };
 
   render() {
     return (
-
-      <form onSubmit={(event) => this.submit(event)}>
-        <TextField
+      <Formsy.Form
+        onValid={this.enableButton}
+        onInvalid={this.disableButton}
+        onValidSubmit={this.submit}
+        onInvalidSubmit={this.notifyFormError}
+      >
+        <FormsyText
           ref="email" type="text" name="email"
+          required
           hintText="email"
-          errorText=""
         />
-        <TextField
+        <FormsyText
           ref="password" type="password" name="password"
+          required
           hintText="password"
-          errorText=""
         />
-        <FlatButton label="Login" type="submit"/>
-      </form>
+        <FlatButton label="Login" type="submit"
+                    disabled={!this.state.canSubmit}
+        />
+
+      </Formsy.Form>
     );
   }
 }
