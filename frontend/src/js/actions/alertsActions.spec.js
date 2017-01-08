4129dd6139b9f6e8047fedb8e3cda101d64e1e8b
npoirey <nicolas.poirey@gmail.com>
2017-01-08 23:57:09 +0100

    Added test coverage on frontend

--- frontend/src/js/actions/alertsActions.spec.js
+++ frontend/src/js/actions/alertsActions.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import chai from 'chai'
-import { showError } from './alertsActions'
+import { showError, showSuccess, reset } from './alertsActions'
 import actions from './actionTypes'
 
 const expect = chai.expect
@@ -13,6 +13,23 @@ describe('alertActions', () => {
         payload: 'this is an error',
       }
       expect(showError('this is an error')).to.eql(expectedAction)
+    })
+  })
+  describe('showSuccess', () => {
+    it('should create an action to show a success', () => {
+      const expectedAction = {
+        type: actions.alerts.success,
+        payload: 'this is a success',
+      }
+      expect(showSuccess('this is a success')).to.eql(expectedAction)
+    })
+  })
+  describe('reset', () => {
+    it('should create an action to reset alert', () => {
+      const expectedAction = {
+        type: actions.alerts.reset,
+      }
+      expect(reset('this is an error')).to.eql(expectedAction)
     })
   })
 })
